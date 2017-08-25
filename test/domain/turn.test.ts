6b1b08e6b5996e0b9ff2d7be912ba2c8544a4e33
yajamon <yajamon.tatsuki@gmail.com>
2017-08-25 18:42:06 +0900

    Add test: Turn#equals()

--- test/domain/turn.test.ts
+++ test/domain/turn.test.ts
@@ -3,6 +3,14 @@ import { Turn } from "../../src/domain/turn"
 
 
 describe("Turn", function(){
+
+    describe("#equals()", function(){
+        it("is returns true when compared by Turn object whose same value", function(){
+            const turn = new Turn(1);
+            expect(turn.equals(new Turn(1))).to.equal(true);
+        });
+    });
+
     describe("#next()", function(){
         it("is returns a Turn object whose value is one greater than the current value", function(){
             const current = new Turn(0);
