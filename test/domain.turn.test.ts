45afbb8b330801f3cb9dfefb1cd286a9deeb0e59
yajamon <yajamon.tatsuki@gmail.com>
2017-08-27 14:55:08 +0900

    Fix test small

--- test/domain.turn.test.ts
+++ test/domain.turn.test.ts
@@ -6,11 +6,11 @@ describe("Turn", function(){
     describe("#equals()", function() {
         it("is returns true when compared by Turn object whose same value", function(){
             const turn = new Turn(1);
-            expect(turn.equals(new Turn(1))).to.equal(true);
+            expect(turn.equals(new Turn(1))).to.be.true;
         });
         it("is returns false when compared by Turn object whose not same value", function(){
             const turn = new Turn(1);
-            expect(turn.equals(new Turn(10))).to.equal(false);
+            expect(turn.equals(new Turn(10))).to.be.false;
         });
     });
 
@@ -18,7 +18,7 @@ describe("Turn", function(){
         it("is returns a Turn object whose value is one greater than the current value", function(){
             const current = new Turn(0);
             const next = current.next();
-            expect(next.equals(new Turn(1))).to.equal(true);
+            expect(next.equals(new Turn(1))).to.be.true;
         });
     });
 });
