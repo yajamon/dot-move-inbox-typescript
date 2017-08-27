08f71a5a1019efd242b149137efd0f1fa3c0b108
yajamon <yajamon.tatsuki@gmail.com>
2017-08-27 15:27:41 +0900

    Use GaemConfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { Game } from "./application/game";
+import { GameConfig } from "./application/gameConfig";
+import { Distance } from "./domain/distance";
+import { Size } from "./domain/size";
 import { ConsoleGameRenderer } from "./presentation/consoleGameRenderer";
 
-const game = new Game(new ConsoleGameRenderer());
+const game = new Game({
+    renderer: new ConsoleGameRenderer(),
+    worldSize: new Size(
+        new Distance(16),
+        new Distance(4),
+    ),
+});
 game.start();
