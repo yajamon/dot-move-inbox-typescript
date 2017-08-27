327dfd6fd8b407371900d57524e7fc22f63a178c
yajamon <yajamon.tatsuki@gmail.com>
2017-08-27 15:57:22 +0900

    Add render method: generateWorld

--- src/presentation/consoleGameRenderer.ts
+++ src/presentation/consoleGameRenderer.ts
@@ -1,4 +1,5 @@
 import { Game } from "../application/game";
+import { World } from "../domain/world";
 import { GameRenderer } from "./gameRenderer";
 
 export class ConsoleGameRenderer implements GameRenderer {
@@ -8,6 +9,22 @@ export class ConsoleGameRenderer implements GameRenderer {
             process.stdout.write("finish.\n");
             return;
         }
+        process.stdout.write(this.generateWorld(game.world));
         process.stdout.write("render turn :" + game.turn.value + "\n");
+    }
+
+    public generateWorld(world: World): string {
+        let lines: string[] = [];
+        for (let row = 0; row < world.size.height.value; ++row) {
+            let line = "";
+            for (let col = 0; col < world.size.width.value; ++col) {
+                line += " ";
+            }
+            lines[row] = line;
+        }
+        lines = lines.map((line) => "|" + line + "|\n");
+        lines.unshift("_".repeat(world.size.width.value + 2) + "\n");
+        lines.push("\"".repeat(world.size.width.value + 2) + "\n");
+        return lines.join("");
     }
 }
