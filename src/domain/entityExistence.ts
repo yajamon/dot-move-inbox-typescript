2274cb6ae16303df7718d7d04cf6e7442a668781
yajamon <yajamon.tatsuki@gmail.com>
2017-08-31 14:49:51 +0900

    Add property of Point to EntityExistence

--- src/domain/entityExistence.ts
+++ src/domain/entityExistence.ts
@@ -1,5 +1,8 @@
 import { Entity } from "./entity";
+import { Point } from "./point";
 export class EntityExistence {
+    public point: Point;
     constructor(public readonly entity: Entity) {
+        this.point = new Point();
     }
 }
