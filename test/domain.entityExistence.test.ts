829065399cfc08f56cf5c3afa6bb2f2a829fd127
yajamon <yajamon.tatsuki@gmail.com>
2017-08-31 14:25:33 +0900

    Add test frame: EntityExistence

---
+++ test/domain.entityExistence.test.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { Entity } from "../src/domain/entity";
+import { EntityExistence } from "../src/domain/entityExistence";
+
+describe("EntityExistence", function(){
+
+    it("has property of Entity named entity", function(){
+        const e  = new Entity();
+        const ex = new EntityExistence(e);
+        expect(ex).to.have.property("entity").that.is.instanceof(Entity);
+    });
+});
