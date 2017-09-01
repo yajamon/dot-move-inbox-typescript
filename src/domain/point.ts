ef8061239c922fba95bd8ffcb133b87030a72b79
yajamon <yajamon.tatsuki@gmail.com>
2017-09-01 16:25:44 +0900

    Add property y to Point

--- src/domain/point.ts
+++ src/domain/point.ts
@@ -1,5 +1,8 @@
 import { Vector } from "./vector";
 export class Point {
-    constructor(readonly x: Vector = new Vector()) {
+    constructor(
+        readonly x: Vector = new Vector(),
+        readonly y: Vector = new Vector(),
+    ) {
     }
 }
