dd6bbc34ff2c92a977800fae77c19b4e14dfa5dc
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 23:50:26 +0900

    Edit test of EntityExistence: constructor

--- test/domain.entityExistence.test.ts
+++ test/domain.entityExistence.test.ts
@@ -6,14 +6,25 @@ import { Point } from "../src/domain/point";
 describe("EntityExistence", function(){
 
     it("has property of Entity named entity", function(){
-        const e  = new Entity();
-        const ex = new EntityExistence(e);
+        const e = new Entity();
+        const p = Point.make(0, 0);
+        const ex = new EntityExistence(e, p);
         expect(ex).to.have.property("entity").that.is.instanceof(Entity);
     });
 
     it("has property of Point named point", function(){
-        const e  = new Entity();
-        const ex = new EntityExistence(e);
+        const e = new Entity();
+        const p = Point.make(0, 0);
+        const ex = new EntityExistence(e, p);
         expect(ex).to.have.property("point").that.is.instanceof(Point);
+    });
+
+    describe("constructor", function() {
+        it("require Entity and Point", function() {
+            const e = new Entity();
+            const p = Point.make(0, 0);
+            const ex = new EntityExistence(e, p);
+            expect(ex).is.instanceof(EntityExistence);
+        });
     });
 });
