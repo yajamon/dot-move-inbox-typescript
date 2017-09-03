3534e01ec770e70f9f6f7848e096f8d9c28068e7
yajamon <yajamon.tatsuki@gmail.com>
2017-09-04 02:28:43 +0900

    Edit World: add method 'addEntity()'

--- src/domain/world.ts
+++ src/domain/world.ts
@@ -1,8 +1,15 @@
 import { Distance } from "./distance";
+import { Entity } from "./entity";
 import { EntityExistence } from "./entityExistence";
+import { Point } from "./point";
 import { Size } from "./size";
 export class World {
     public readonly entityExistence: EntityExistence[] = [];
     constructor(readonly size: Size) {
+    }
+
+    public addEntry(entity: Entity, point: Point) {
+        const e = new EntityExistence(entity, point);
+        this.entityExistence.push(e);
     }
 }
