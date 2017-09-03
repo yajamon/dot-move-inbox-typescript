5d2bb288470f9842843e59d505592e1f76d42714
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 23:53:30 +0900

    Edit EntityExistence: require Point when construct

--- src/domain/entityExistence.ts
+++ src/domain/entityExistence.ts
@@ -2,8 +2,6 @@ import { Entity } from "./entity";
 import { Point } from "./point";
 import { Vector } from "./vector";
 export class EntityExistence {
-    public point: Point;
-    constructor(public readonly entity: Entity) {
-        this.point = new Point(new Vector(0));
+    constructor(public readonly entity: Entity, public point: Point) {
     }
 }
