feff18d0cbe59998e72a710f743241319b13b015
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 14:34:25 +0900

    Add property of EntityExistence[] to World

--- src/domain/world.ts
+++ src/domain/world.ts
@@ -1,6 +1,8 @@
 import { Distance } from "./distance";
+import { EntityExistence } from "./entityExistence";
 import { Size } from "./size";
 export class World {
+    public readonly entityExistence: EntityExistence[] = [];
     constructor(readonly size: Size) {
     }
 }
