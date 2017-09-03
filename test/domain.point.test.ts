d54d3fbeedac3ac63342e0192332bc4562391d76
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 22:24:34 +0900

    Add test to Point: make()

--- test/domain.point.test.ts
+++ test/domain.point.test.ts
@@ -11,4 +11,13 @@ describe("Point", function(){
         const point = new Point();
         expect(point).to.have.property("y").that.is.instanceof(Vector);
     });
+
+    describe(".make()", function() {
+        it("make Point with number", function() {
+            const x = 1;
+            const y = 2;
+            const point = Point.make(x, y);
+            expect(point).instanceOf(Point);
+        });
+    });
 });
