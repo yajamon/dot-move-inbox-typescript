dc40dc8e121a8677f91a07d143e956836127692b
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 14:10:07 +0900

    Add test: expect world to have property size

---
+++ test/domain.world.test.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import { Distance } from "../src/domain/distance";
+import { Size } from "../src/domain/size";
+import { World } from "../src/domain/world";
+
+describe("World", function() {
+
+    it("has property Size named size", function() {
+        const size = new Size(new Distance(8), new Distance(3));
+        const world = new World(size);
+        expect(world).to.have.property("size").that.is.instanceof(Size);
+    });
+});
